02ce7d7eb09b1a446816f7ed4e3b0ba2a0b8e5c8
Marc Andreu <marc@itqualab.com>
2022-04-22 13:35:40 +0900

    docs: This is a long long long commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,15 @@
-module.exports = { 
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'header-max-length': [2, 'always', 150],
-      }
+module.exports = {
 
+    //extends: ['@commitlint/config-conventional'],
+
+    /*
+     * Any rules defined here will override rules from @commitlint/config-conventional
+     */
+    rules: {
+        //'scope-case': [0, 'always', 'lower-case'],
+        //'subject-case': [0, 'always', 'lower-case'],
+        'header-max-length': [2, 'always', 150]
+    },
+
+    extends: ['@commitlint/config-conventional'],
 };
