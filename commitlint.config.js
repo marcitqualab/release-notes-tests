e1671838a4af0caf1a33388e6023b09f59e39164
Marc Andreu <marc@itqualab.com>
2022-04-22 13:42:02 +0900

    fix: test this

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,15 +1,12 @@
 module.exports = {
 
-    //extends: ['@commitlint/config-conventional'],
-
     /*
      * Any rules defined here will override rules from @commitlint/config-conventional
      */
-    rules: {
-        //'scope-case': [0, 'always', 'lower-case'],
+    rules: {        
         'subject-case': [0, 'always', 'lower-case'],
         'header-max-length': [2, 'always', 150]
     },
 
-    extends: ['@commitlint/config-conventional'],
+    extends: ['@commitlint/config-conventional']
 };
